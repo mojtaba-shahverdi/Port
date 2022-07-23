7de9aebdd6412e183aae4a511855f8767e365c48
Mojtaba Shahverdi <91985158+mojtaba-shahverdi@users.noreply.github.com>
2022-07-23 04:44:06 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
